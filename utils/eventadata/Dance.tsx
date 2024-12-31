1bfad04463d4dad0bdecbf12eb62b176ad5a3c50
harshitnitjsr <harshit26092004@gmail.com>
2024-12-31 14:15:25 +0530

    responsiveness

--- utils/eventadata/Dance.tsx
+++ utils/eventadata/Dance.tsx
@@ -12,12 +12,12 @@ export const DummyContent = () => {
             className="mb-4 rounded-3xl bg-[#F5F5F7] p-6 md:p-10 lg:p-14"
             key={"dummy-content" + index}
           >
-            <div className="relative inline-block w-max text-center font-AnotherFont text-3xl [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))] md:text-4xl lg:text-5xl">
-              <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat py-3 text-center text-5xl text-orange-500 text-transparent [text-shadow:0_0_rgba(0,0,0,0.1)] md:text-6xl lg:text-7xl">
-                {item.eventname}
+            <div className="relative text-center font-AnotherFont text-3xl [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))] md:text-4xl lg:text-5xl">
+              <div className="absolute left-0 top-[1px] w-full bg-clip-text bg-no-repeat py-3 text-center text-5xl text-orange-500 text-transparent [text-shadow:0_0_rgba(0,0,0,0.1)] md:text-6xl lg:text-7xl">
+                <span className="block break-words">{item.eventname}</span>
               </div>
-              <div className="relative bg-gradient-to-r bg-clip-text bg-no-repeat py-3 text-center text-5xl text-transparent text-yellow-600 md:text-6xl lg:text-7xl">
-                {item.eventname}
+              <div className="relative w-full bg-gradient-to-r bg-clip-text bg-no-repeat py-3 text-center text-5xl text-transparent text-yellow-600 md:text-6xl lg:text-7xl">
+                <span className="block break-words">{item.eventname}</span>
               </div>
             </div>
 
@@ -26,7 +26,7 @@ export const DummyContent = () => {
               alt={item.eventname + " mockup"} // Updated alt text for better accessibility
               height={1000} 
               width={1000} 
-              className="mx-auto w-full object-contain sm:w-3/4 md:w-2/3 lg:w-2/3" // Adjust responsiveness
+              className="mx-auto w-full object-contain sm:w-3/4 md:w-2/3 lg:w-2/3" 
             />
 
             <MovingBorderDemo item={item} />
@@ -38,3 +38,4 @@ export const DummyContent = () => {
     </div>
   );
 };
+
